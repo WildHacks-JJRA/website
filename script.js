b3c90d518f4ab8d6a6ff2743142714a93afdb148
Jung Oh <jung3o@yahoo.com>
2014-11-22 15:35:42 -0600

    init

---
+++ script.js
@@ -0,0 +1,58 @@
+var example = [
+    [0, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 1, 1, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1]
+];
+
+var boxArray = example;
+
+var boxSize = 50;
+var strokeSize = 2;
+
+var stage = new Kinetic.Stage({
+container: 'container',
+    width: 800,
+    height: 800
+});
+var shapesLayer = new Kinetic.Layer();
+
+var boxGroup = new Kinetic.Group({
+    x: 0,
+    y: 0,
+});
+
+for(var y = 0; y < boxArray.length; y++) {
+    for(var x = 0; x < boxArray[0].length; x++) {
+
+        switch(boxArray[y][x]) {
+            case(0): // space
+                var box = new Kinetic.Rect({
+                    x: x * boxSize,
+                    y: y * boxSize,
+                    width: boxSize,
+                    height: boxSize,
+                    fill: 'white',
+                    stroke: 'black',
+                    strokeWidth: strokeSize
+                });
+                break;
+            case(1): // wall
+                var box = new Kinetic.Rect({
+                    x: x * boxSize,
+                    y: y * boxSize,
+                    width: boxSize,
+                    height: boxSize,
+                    fill: 'black'
+                });
+                break;
+        }
+
+        boxGroup.add(box);
+    }
+}
+
+shapesLayer.add(boxGroup);
+
+stage.add(shapesLayer);
