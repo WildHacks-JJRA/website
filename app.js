6337be1694d1c1c9bea5c25e032f6435c863efd7
Jung Oh <jung3o@yahoo.com>
2014-11-22 22:26:03 -0600

    server finished?

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var maze = {
     rmaze: [],
     dmaze: []
 };
+var bomb = null;
 
 http.listen(80);
 
@@ -30,45 +31,27 @@ var clickRadius = 1;
 
 var StringDecoder = require('string_decoder').StringDecoder;
 var decoder = new StringDecoder('utf8');
+var change;
 
 net.createServer(function(c) {
 
-    c.on('data', function(data) {
-        var string = decoder.write(data);
-        var stringType = string.split('\n');
-
-        if(stringType[0] == "maze") {
-
-            var mazeSettings = string.match(/.*?:(.*)/g);
-            var setting;
-
-            for(var i = 0; i < mazeSettings.length; i++) {
-                setting = mazeSettings[i].split(':');
-                if(setting[0] == 'size') {
-                    maze.size = setting[1];
-                } else if(setting[0] == 'rmaze') { // left/right
-                    for(var y = 0; y < maze.size; y++) {
-                        maze.rmaze[y] = [];
-                        for(var x = 0; x < maze.size; x++) {
-                            maze.rmaze[y][x] = setting[1].substr(x * maze.size + y, 1);
-                        }
-                    }
-                } else if(setting[0] == 'dmaze') { // up/down
-                    for(var y = 0; y < maze.size; y++) {
-                        maze.dmaze[y] = [];
-                        for(var x = 0; x < maze.size; x++) {
-                            maze.dmaze[y][x] = setting[1].substr(x * maze.size + y, 1);
-                        }
-                    }
-                }
-            }
-        } else {
-
-        }
-    });
-
     io.on('connection', function (socket) {
-        socket.emit('maze', maze);
+
+        change = c.on('data', parseData);
+        switch(change) {
+            case 'maze':
+                socket.emit('maze', maze);
+                break;
+            case 'bomb':
+            /* bomb
+                0 - miss
+                1 - almost
+                2 - hit
+                3 - over
+             */
+                socket.emit('bomb', bomb);
+                break;
+        }
 
         socket.on('click', function (data) {
 
@@ -81,3 +64,40 @@ net.createServer(function(c) {
     });
     c.pipe(c);
 }).listen(5000);
+
+function parseData(data) {
+    var string = decoder.write(data);
+    var stringType = string.split('\n');
+
+    if(stringType[0] == "maze") {
+
+        var mazeSettings = string.match(/.*?:(.*)/g);
+        var setting;
+
+        for(var i = 0; i < mazeSettings.length; i++) {
+            setting = mazeSettings[i].split(':');
+            if(setting[0] == 'size') {
+                maze.size = setting[1];
+            } else if(setting[0] == 'rmaze') { // left/right
+                for(var y = 0; y < maze.size; y++) {
+                    maze.rmaze[y] = [];
+                    for(var x = 0; x < maze.size; x++) {
+                        maze.rmaze[y][x] = setting[1].substr(x * maze.size + y, 1);
+                    }
+                }
+            } else if(setting[0] == 'dmaze') { // up/down
+                for(var y = 0; y < maze.size; y++) {
+                    maze.dmaze[y] = [];
+                    for(var x = 0; x < maze.size; x++) {
+                        maze.dmaze[y][x] = setting[1].substr(x * maze.size + y, 1);
+                    }
+                }
+            }
+        }
+        return 'maze';
+    } else if(stringType[0] == "bomb") {
+        bomb = stringType[1];
+        return 'bomb';
+    }
+    return false;
+}
