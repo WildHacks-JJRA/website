1a2976bccde4075804e86f20072f594dd0fc1c87
Jung Oh <jung3o@yahoo.com>
2014-11-22 22:10:16 -0600

    YAY

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var path = require('path');
 
 var net = require('net');
 
+var maze = {
+    size: null,
+    rmaze: [],
+    dmaze: []
+};
+
 http.listen(80);
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,53 +28,54 @@ var clickPos = {
 }
 var clickRadius = 1;
 
-io.on('connection', function (socket) {
-    socket.on('p1 move', function (data) {
-        playerPos = {
-            x: Math.floor(data.x),
-            y: Math.floor(data.y)
-        }
-    });
-    socket.on('p2 click', function (data) {
-
-        clickPos = {
-            x: Math.floor(data.x),
-            y: Math.floor(data.y)
-        }
-
-        console.log(clickPos);
-
-        //Win
-        if(playerPos.x == clickPos.x &&
-           playerPos.y == clickPos.y) {
-            socket.emit('p1 endGame', true);
-            socket.emit('p2 endGame', true);
-        } else if(playerNearClick()) {
-            socket.emit('p1 closeClick', true);
-            socket.emit('p2 closeClick', true);
-        }
-    });
-});
-
-function playerNearClick() {
-    for(var x = clickPos.x - clickRadius; x <= clickPos.x + clickRadius; x++) {
-        for(var y = clickPos.y - clickRadius; y <= clickPos.y + clickRadius; y++) {
-            if(playerPos.x == x &&
-               playerPos.y == y) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
 var StringDecoder = require('string_decoder').StringDecoder;
 var decoder = new StringDecoder('utf8');
 
 net.createServer(function(c) {
+
     c.on('data', function(data) {
-        console.log(decoder.write(data));
+        var string = decoder.write(data);
+        var stringType = string.split('\n');
+
+        if(stringType[0] == "maze") {
+
+            var mazeSettings = string.match(/.*?:(.*)/g);
+            var setting;
+
+            for(var i = 0; i < mazeSettings.length; i++) {
+                setting = mazeSettings[i].split(':');
+                if(setting[0] == 'size') {
+                    maze.size = setting[1];
+                } else if(setting[0] == 'rmaze') { // left/right
+                    for(var y = 0; y < maze.size; y++) {
+                        maze.rmaze[y] = [];
+                        for(var x = 0; x < maze.size; x++) {
+                            maze.rmaze[y][x] = setting[1].substr(x * maze.size + y, 1);
+                        }
+                    }
+                } else if(setting[0] == 'dmaze') { // up/down
+                    for(var y = 0; y < maze.size; y++) {
+                        maze.dmaze[y] = [];
+                        for(var x = 0; x < maze.size; x++) {
+                            maze.dmaze[y][x] = setting[1].substr(x * maze.size + y, 1);
+                        }
+                    }
+                }
+            }
+            socket.emit('maze', maze);
+        } else {
+
+        }
     });
-    c.write('hello\r\n');
+    io.on('connection', function (socket) {
+        socket.on('click', function (data) {
+
+            clickPos = {
+                x: Math.floor(data.x),
+                y: Math.floor(data.y)
+            }
+            c.write(clickPos.x+','+clickPos.y);
+        });
+    });
     c.pipe(c);
 }).listen(5000);
