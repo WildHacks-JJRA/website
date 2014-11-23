5877b89288551cd0c4c25f607732b08766cfd801
Jung Oh <jung3o@yahoo.com>
2014-11-22 18:31:45 -0600

    server works, need game inorder to continue

--- app.js
+++ app.js
@@ -10,37 +10,49 @@ http.listen(8080);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var playerOnePos = {
+var playerPos = {
     x: 0,
     y: 0
 };
+var clickPos = {
+    x: null,
+    y: null
+}
 var clickRadius = 1;
 
 io.on('connection', function (socket) {
     socket.on('p1 move', function (data) {
-        playerPos.x = Math.ceil(data.x);
-        playerPos.y = Math.ceil(data.y);
+        playerPos = {
+            x: Math.floor(data.x),
+            y: Math.floor(data.y)
+        }
     });
     socket.on('p2 click', function (data) {
 
-        //Win
-        if(playerPos.x == Math.ceil(data.x) &&
-           playerPos.y == Math.ceil(data.y)) {
-            // socket.emit('')
+        clickPos = {
+            x: Math.floor(data.x),
+            y: Math.floor(data.y)
         }
 
-        //Close
-        if(playerNearClick(data)) {
+        console.log(clickPos);
+
+        //Win
+        if(playerPos.x == clickPos.x &&
+           playerPos.y == clickPos.y) {
+            socket.emit('p1 endGame', true);
+            socket.emit('p2 endGame', true);
+        } else if(playerNearClick()) {
             socket.emit('p1 closeClick', true);
-            socket.emite('p2 closeClick', true);
+            socket.emit('p2 closeClick', true);
         }
     });
 });
 
-function playerNearClick(clickPos) {
+function playerNearClick() {
     for(var x = clickPos.x - clickRadius; x <= clickPos.x + clickRadius; x++) {
         for(var y = clickPos.y - clickRadius; y <= clickPos.y + clickRadius; y++) {
-            if(playerPos.x == x && playerPos.y) {
+            if(playerPos.x == x &&
+               playerPos.y == y) {
                 return true;
             }
         }
--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,24 @@
 var socket = io.connect('http://localhost');
-socket.on('p2 closeClick', function (data) { //
+socket.on('p2 endGame', function (data) {
+    console.log('You win!');
+    // when the click was close to the player
+});
+
+socket.on('p2 closeClick', function (data) {
+    console.log('ping!');
+    // when the click was close to the player
+});
+
+// Detect mouse pos and send x,y points
+var $container = $("#container");
+$container.on('click', function(e) {
+    console.log({
+        x: e.offsetX / boxSize,
+        y: e.offsetY / boxSize
+    });
+
+    socket.emit('p2 click', {
+        x: e.offsetX / boxSize,
+        y: e.offsetY / boxSize
+    });
 });
