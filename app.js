287e1b437cf8e405da7ddae6c455103292ac86c5
Jung Oh <jung3o@yahoo.com>
2014-11-22 20:58:46 -0600

    test

--- app.js
+++ app.js
@@ -8,47 +8,47 @@ var path = require('path');
 
 var net = require('net');
 
-http.listen(80);
+// http.listen(80);
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-var playerPos = {
-    x: 0,
-    y: 0
-};
-var clickPos = {
-    x: null,
-    y: null
-}
-var clickRadius = 1;
+// var playerPos = {
+//     x: 0,
+//     y: 0
+// };
+// var clickPos = {
+//     x: null,
+//     y: null
+// }
+// var clickRadius = 1;
 
-io.on('connection', function (socket) {
-    socket.on('p1 move', function (data) {
-        playerPos = {
-            x: Math.floor(data.x),
-            y: Math.floor(data.y)
-        }
-    });
-    socket.on('p2 click', function (data) {
+// io.on('connection', function (socket) {
+//     socket.on('p1 move', function (data) {
+//         playerPos = {
+//             x: Math.floor(data.x),
+//             y: Math.floor(data.y)
+//         }
+//     });
+//     socket.on('p2 click', function (data) {
 
-        clickPos = {
-            x: Math.floor(data.x),
-            y: Math.floor(data.y)
-        }
+//         clickPos = {
+//             x: Math.floor(data.x),
+//             y: Math.floor(data.y)
+//         }
 
-        console.log(clickPos);
+//         console.log(clickPos);
 
-        //Win
-        if(playerPos.x == clickPos.x &&
-           playerPos.y == clickPos.y) {
-            socket.emit('p1 endGame', true);
-            socket.emit('p2 endGame', true);
-        } else if(playerNearClick()) {
-            socket.emit('p1 closeClick', true);
-            socket.emit('p2 closeClick', true);
-        }
-    });
-});
+//         //Win
+//         if(playerPos.x == clickPos.x &&
+//            playerPos.y == clickPos.y) {
+//             socket.emit('p1 endGame', true);
+//             socket.emit('p2 endGame', true);
+//         } else if(playerNearClick()) {
+//             socket.emit('p1 closeClick', true);
+//             socket.emit('p2 closeClick', true);
+//         }
+//     });
+// });
 
 function playerNearClick() {
     for(var x = clickPos.x - clickRadius; x <= clickPos.x + clickRadius; x++) {
